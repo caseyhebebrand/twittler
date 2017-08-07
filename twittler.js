02647c3e86bb14504d972e9d730e2659621f2c58
Casey Hebebrand <caseyhebebrand@gmail.com>
2017-08-06 22:41:05 -0700

    "Automatically update Tweets and include date stamp"

--- twittler.js
+++ twittler.js
@@ -1,16 +1,23 @@
 $(document).ready(function() {
-
-
-        var $tweetFeed = $('.tweetFeed');
+	var $tweetFeed = $('.tweetFeed');
         $tweetFeed.html('');
 
-        var index = streams.home.length - 1;
-        while(index >= 0){
+    function updateTweets() {
+    	$tweetFeed.html('')
+    	var streamLength = streams.home.length;
+    	var maxFeedLength = 20;
+        var index = streamLength - 1;
+        var stopIndex = streamLength > maxFeedLength ?  streamLength - maxFeedLength - 1 : 0;
+        while(index >= stopIndex){
           var tweet = streams.home[index];
-          var $tweet = $('<div></div>');
-          $tweet.text('@' + tweet.user + ': ' + tweet.message);
-          $tweet.appendTo($tweetFeed);
+          var $tweetDiv = $('<div></div>');
+          $tweetDiv.text('@' + tweet.user + ' (' + tweet.created_at + '): ' + tweet.message);
+
+          $tweetDiv.appendTo($tweetFeed);
           index -= 1;
         }
+        setTimeout(updateTweets, 5000);
+    }
 
+    updateTweets();    
 });
