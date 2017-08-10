d2fb26d7aa398b42952547e3407083194e79693b
Casey Hebebrand <caseyhebebrand@gmail.com>
2017-08-09 19:51:09 -0700

    'Refactor js and refine css'

--- twittler.js
+++ twittler.js
@@ -1,90 +1,93 @@
 $(document).ready(function() {
-	var $tweetFeed = $('#tweetFeed');
-        $tweetFeed.html('');
-    var $users = $('#users')
-    var $userFeed = $('#userfeed');
-    var tweetUser = $('')
+  var $homePage = $('#homePage');
+	var $homeFeed = $('#homeFeed');
+  var $userPage = $('#userPage')
+  var $userFeed = $('#userFeed');
 
-    function updateTweets() {
-    	$tweetFeed.html('')
-    	var streamLength = streams.home.length;
-    	var maxFeedLength = 20;
-        var index = streamLength - 1;
-        var stopIndex = streamLength > maxFeedLength ?  streamLength - maxFeedLength - 1 : 0;
-        while(index >= stopIndex){
-          var tweet = streams.home[index];
-          var $tweetDiv = $('<div></div>');
-          var $tweetTime = $('<h6></h6>');
-          var $tweetMessage = $('<p></p>');
-          var $tweetUser = $('<a>tweet.user</a>');
-          $tweetUser.data('userNameVal', tweet.user);
-          $tweetUser.text('@' + tweet.user);
-          var month = tweet.created_at.getMonth() + 1;
-          var day = tweet.created_at.getDate();
-          var year = tweet.created_at.getFullYear();
-          var hour = tweet.created_at.getHours() === 0 ? 12 : tweet.created_at.getHours() % 12;
-          var minute = tweet.created_at.getMinutes() >= 10 ? tweet.created_at.getMinutes() : "0" + tweet.created_at.getMinutes().toString();
-          var ampm = tweet.created_at.getHours() < 12 ? "am" : "pm";
-          $tweetTime.text(month + '/' + day + '/' + year + ' ' + hour + ':' + minute + ampm);
-          $tweetMessage.text(tweet.message);
+  function constructTweetDiv(tweet) {
+    // Construct html for tweet user
+    var $tweetUser = $('<a>tweet.user</a>');
+    $tweetUser.data('userNameVal', tweet.user);
+    $tweetUser.text('@' + tweet.user); 
 
-          $tweetUser.appendTo($tweetDiv);
-          $tweetTime.appendTo($tweetDiv);
-          $tweetMessage.appendTo($tweetDiv);
+    // Construct html for tweet time
+    var $tweetTime = $('<h6></h6>');
+    var month = tweet.created_at.getMonth() + 1;
+    var day = tweet.created_at.getDate();
+    var year = tweet.created_at.getFullYear();
+    var hour = tweet.created_at.getHours() === 0 ? 12 : tweet.created_at.getHours() % 12;
+    var minute = tweet.created_at.getMinutes() >= 10 ? tweet.created_at.getMinutes() : "0" + tweet.created_at.getMinutes().toString();
+    var ampm = tweet.created_at.getHours() < 12 ? "am" : "pm";
+    $tweetTime.text(month + '/' + day + '/' + year + ' ' + hour + ':' + minute + ampm);
 
-          $tweetDiv.appendTo($tweetFeed);
-          index -= 1;
-        }
-        setTimeout(updateTweets, 20000);
+     // Construct html for tweet message
+    var $tweetMessage = $('<p></p>');
+    $tweetMessage.text(tweet.message);
+
+    // Build tweet div
+    var $tweetDiv = $('<div></div>');
+    // Add the 'tweetUser', 'tweetTime', and 'tweetMessage' to the 'tweetDiv'
+    $tweetUser.appendTo($tweetDiv);
+    $tweetTime.appendTo($tweetDiv);
+    $tweetMessage.appendTo($tweetDiv);
+
+    return $tweetDiv;
+  }
+
+  function updateHomeFeedTweets() {
+    // Clear the home feed
+  	$homeFeed.html('')
+
+    // Defining length of homeFeed
+    var maxFeedLength = 20;
+
+  	var streamLength = streams.home.length;
+    var index = streamLength - 1;
+    var stopIndex = streamLength > maxFeedLength ?  streamLength - maxFeedLength - 1 : 0;
+
+    // Add most recent 'maxFeedLength' tweets to homeFeed
+    while(index >= stopIndex){
+      // Grab a tweet from the tweet stream
+      var tweet = streams.home[index];
+
+      // Add the tweetDiv to the 'homeFeed'
+      constructTweetDiv(tweet).appendTo($homeFeed);
+      index -= 1;
     }
+    setTimeout(updateHomeFeedTweets, 20000);
+  }
+  updateHomeFeedTweets();    
 
-    updateTweets();    
+  function showUserFeed(selectedUser) {
+    // Clear the 'userFeed'
+    $userFeed.html('');
 
-    function showUserFeed(selectedUser) {
-    	$tweetFeed.hide().fadeOut();
-    	var $userPage = $('<div></div>')
-    	$userPage.text('Welcome to ' + selectedUser + '\'s Page!')
-    	$userPage.appendTo($userFeed)
-    	var $userTweets = $('<div></div>')
+    // Hide homePage
+  	$homePage.hide().fadeOut();
 
-    	var allUserTweets = streams.users[selectedUser];
-    	allUserTweets.forEach(function(tweet) {
-    		var $userTime = $('<h6></h6>');
-    		var $userMessage = $('<p></p>');
-    		var $userName = $('<a>tweet.user</a>');
+    // Update 'userWelcome' text to greet 'selectedUser'
+  	var $userWelcome = $('#userWelcome') 
+  	$userWelcome.text('Welcome to ' + selectedUser + '\'s Page!')
 
-    		$userName.text('@' + tweet.user);
-    		var month = tweet.created_at.getMonth() + 1;
-          	var day = tweet.created_at.getDate();
-          	var year = tweet.created_at.getFullYear();
-          	var hour = tweet.created_at.getHours() === 0 ? 12 : tweet.created_at.getHours() % 12;
-          	var minute = tweet.created_at.getMinutes() >= 10 ? tweet.created_at.getMinutes() : "0" + tweet.created_at.getMinutes().toString();
-          	var ampm = tweet.created_at.getHours() < 12 ? "am" : "pm";
-          	$userTime.text(month + '/' + day + '/' + year + ' ' + hour + ':' + minute + ampm);
-          	$userMessage.text(tweet.message);
+    // Grab all the tweets for the 'selectedUser' from the tweet stream
+  	var allUserTweets = streams.users[selectedUser];
 
-    		$userName.appendTo($userTweets);
-    		$userTime.appendTo($userTweets);
-    		$userMessage.appendTo($userTweets);
+    // Add all user tweets to the 'userFeed'
+  	allUserTweets.forEach(function(tweet) {
+      // Add the tweetDiv to the 'userFeed'
+      constructTweetDiv(tweet).appendTo($userFeed);
+  	});
+    // Show the 'userPage'
+  	$userPage.show().fadeIn();
+  }
 
-    		$userTweets.appendTo($userFeed);
-    	});
-    	$users.show().fadeIn();
-    }
+  $homeFeed.on('click', 'a', function() {
+  	var user = $(this).data('userNameVal')
+  	showUserFeed(user);
+  })
 
-
-    $tweetFeed.on('click', 'a', function() {
-    	//var user = $(this).text();
-    	//var username = user.slice(1, user.length);
-    	var user = $(this).data('userNameVal')
-    	showUserFeed(user);
-    })
-
-   $('#users').on('click', 'button', function() {
-   		$userFeed.html('')
-   		$users.hide().fadeOut();
-   		$tweetFeed.show().fadeIn();
-   })
-
-    
+  $('#userPageBackButton').click(function() {
+   	$userPage.hide().fadeOut();
+   	$homePage.show().fadeIn();
+  }) 
 });
